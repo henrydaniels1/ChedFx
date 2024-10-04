c814703b6b658fb68e7ff589b48ca46278091f9c
henrydaniels1 <henrydanielchibuzor@gmail.com>
2024-10-04 15:38:58 +0100

    new 3--2

--- src/components/FxComponents/FxHeader.jsx
+++ src/components/FxComponents/FxHeader.jsx
@@ -1,6 +1,8 @@
 import { motion, useInView } from 'framer-motion';
 import { useRef } from 'react';
 import '../../style/Fxstyle.css';
+import Cha from '../../assets/Cha.jpg'
+import Cha2 from '../../assets/chartvid.mp4'
 
     
 
@@ -52,7 +54,7 @@ const FxHeader = () => {
         <div className="flex md:justify-between md:flex-row flex-col md:items-center ">
           {/* Text Section with animation */ }
           
-          <motion.div className="w-full md:w-1/2 pt-20 md:pt-0 z-30" variants={textVariants}>
+          <motion.div className="w-full md:w-1/2 text-center md:text-left pt-20 md:pt-0 z-30" variants={textVariants}>
             <motion.div className="space-y-8  pt-8">
            
               <motion.h1
@@ -74,21 +76,17 @@ const FxHeader = () => {
 
            
               <motion.p
-                className=" text-xl text-justify text-gray-400 dark:text-gray-400 md:text-lg lg:text-xl"
+                className=" text-xl text-center text-gray-400  md:text-left dark:text-gray-400 md:text-lg lg:text-xl"
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ delay: 0.3, duration: 0.8 }}
               >
-                {/* Unlock the secrets of forex trading with <br/>our comprehensive courses and expert guidance.<br/>
-                Start your journey to financial freedom today! */}
-                {/* Unlock the secrets of forex trading with our comprehensive courses and expert guidance. Start your
-                journey to financial freedom today! Additionally, we offer a trading bot designed to help you automate
-                your trading strategies and enhance your trading experience. */}
-                Discover forex trading through our comprehensive courses and expert guidance.
+            
+                Discover forex trading through our expert guidance.
                 Begin your journey to financial freedom today, and take advantage of our trading bot to automate your trading strategies.
               </motion.p>
         <div>
-          <button className="btn-6 z-2 px-4 py-2 rounded-2xl text-xl">Get Started</button>
+          <button className="btn-6 z-2 px-4 mx-auto md:mx-0 py-2 rounded-2xl text-xl">Get Started</button>
         </div>
         
             </motion.div>
@@ -96,17 +94,21 @@ const FxHeader = () => {
           </motion.div>
 
           {/* Image Section with animation */}
-          <div className="md:flex rounded-lg md:items-center lg:justify-end w-full mt-8  md:w-1/2">
+          <div className="md:flex rounded-lg hidden md:items-center lg:justify-end w-full mt-8  md:w-1/2">
             <motion.div
               className="relative"
               variants={imageVariants}
             >
               <div className="w-full h-[18rem] lg:w-[35rem] object-cover md:h-[25rem] lg:h-[28rem]">
-                <motion.img
-                  className="h-full w-full object-cover object-center rounded-lg shadow-lg"
-                  src="https://images.stockcake.com/public/e/6/b/e6b9e7f1-c203-4ab0-afb1-b19c2c0b36bf/global-financial-integration-stockcake.jpg"
-                  alt="Trading Image"
-                />
+               <motion.video className="h-full w-full object-cover object-center rounded-lg shadow-lg"
+  src={Cha2}
+  alt="Trading Video"
+  muted      // Video will be muted by default
+  loop       // Video will loop infinitely
+  autoPlay   // Video will start playing automatically
+  playsInline // Allows inline play on mobile devices
+/>
+
               </div>
             </motion.div>
           </div>
@@ -118,11 +120,12 @@ const FxHeader = () => {
       <div className="animated-gradient-background absolute top-0 right-0 bottom-0 left-0 z-[-1] overflow-hidden">
          <motion.img
           className="w-full css-selector h-full object-cover opacity-5"
-          src="https://images.stockcake.com/public/e/6/b/e6b9e7f1-c203-4ab0-afb1-b19c2c0b36bf/global-financial-integration-stockcake.jpg"
+          // src="https://images.stockcake.com/public/e/6/b/e6b9e7f1-c203-4ab0-afb1-b19c2c0b36bf/global-financial-integration-stockcake.jpg"
+          src={Cha}
           alt="Background"
           initial={{ scale: 1 }}
           animate={{ scale: 1.2 }}
-          transition={{ duration: 10, ease: 'easeInOut', repeat: Infinity, repeatType: 'reverse' }}
+          transition={{ duration: 4, ease: 'easeInOut', repeat: Infinity, repeatType: 'reverse' }}
         />
       </div>
     </header>
--- src/components/FxComponents/FxHome.jsx
+++ src/components/FxComponents/FxHome.jsx
@@ -31,7 +31,7 @@ export default function FxHome() {
           {/* <TickSubscriber /> */ }
           {/* <MainBot/> */}
         
-        
+     
           <Pricing/>
           <Faq />
           <Contact/>
