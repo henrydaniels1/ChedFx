ab93c52003cc693a612ec3b45e4c9ac82fd57c74
henrydaniels1 <henrydanielchibuzor@gmail.com>
2025-02-20 10:26:53 +0100

    added idea icon

--- src/components/FxComponents/Faq.jsx
+++ src/components/FxComponents/Faq.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import { useState } from 'react';
 import '../../style/Fxstyle.css';
-// import net from '../../assets/faq1.svg'
+import eart from '../../assets/idea.svg'
 
 
 
@@ -52,7 +52,7 @@ export const Faq = () => {
           <div className="max-w-xl md:mx-auto sm:text-center lg:max-w-2xl relative">
 
             <div className="reveal3 absolute w-20 h-20 md:top-[-1rem] md:left-[2rem] top-[-50%]">
-              <img src="https://static.shuffle.dev/components/preview/88e9307f-3fef-4ff5-a095-3ea7d814a416/assets/public/flex-ui-assets/elements/circle3-yellow.svg" /></div>
+              <img src={eart} /></div>
 
             <h2 className="max-w-lg mb-3 md:text-4xl lg:text-5xl text-3xl  text-center font-bold reveal1 leading-none tracking-tight text-gray-900 md:mx-auto">
              
